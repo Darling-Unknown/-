e22aeb338f922d1d8919ae2c32304fdeb587e5d0
Darling-Unknown <pauluchechukwudarlington@gmail.com>
2025-01-16 11:50:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'unknown',
+  author: 'Unknown',
+  botName: "Unknown",
+  botOwner: 'Unknown', // Your name
+  ownerNumber: '7037839853', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
