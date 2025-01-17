11efa39c6ce2286e64150f147d121c13aa785027
Darling-Unknown <pauluchechukwudarlington@gmail.com>
2025-01-17 23:07:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'unknown',
-  author: 'Unknown',
-  botName: "Unknown",
-  botOwner: 'Unknown', // Your name
-  ownerNumber: '7037839853', //Your number
+  author: 'Unkown',
+  botName: "Unknown Web$",
+  botOwner: 'Unknown_@UnknownWeb_D', // Your name
+  ownerNumber: '07037839853', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  description: "This Is owned By UNKNOWN and Managed By Unknown_WedD.",
+  version: "1.0.5",
 };
 
 module.exports = settings;
