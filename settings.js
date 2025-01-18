3c7314c11666f2865a448b508f91f8c1113e5177
Darling-Unknown <pauluchechukwudarlington@gmail.com>
2025-01-18 18:52:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'Unknown_@UnknownWeb_D', // Your name
   ownerNumber: '07037839853', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This Is owned By UNKNOWN and Managed By Unknown_WedD.",
   version: "1.0.5",
 };
